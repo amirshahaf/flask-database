ef6ecfa2a70b0c9d1807e2c2e1ba3189cc510c09
irmir10 <60602674+irmir10@users.noreply.github.com>
2020-02-27 15:30:32 +0200

    Update server.py

--- server.py
+++ server.py
@@ -56,3 +56,8 @@ def delete():
             print('Something went wrong!')
             print(e)
     return redirect('/')
+
+if __name__ == '__main__':
+        import os  
+        port = int(os.environ.get('PORT', 5000)) 
+        app.run(host='0.0.0.0', port=port)
